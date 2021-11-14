2a8dd39f8ba11562d14c6a940f253a3d182a288a
thechoute <82709682+thechoute@users.noreply.github.com>
2021-11-14 13:17:46 -0400

    🔥 𝕿𝖍ٌ𝖊𝕮𝖍𝖔𝖚𝖙𝖊 🔥

---
+++ main.js
@@ -0,0 +1,112 @@
+const { WAConnection, 
+       MessageType, 
+       Mimetype, 
+      } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const prefix = '.'
+const conn = require("./lib/index")
+conn.connect()
+const client = conn.client
+client.on('chat-update', async (choute) => {
+try {	  
+if (!mek.hasNewMessage) return
+if (!mek.messages) return
+if (mek.key && mek.key.remoteJid == 'status@broadcast') return
+
+choute = mek.messages.all()[0]
+if (!mek.message) return
+global.blocked
+mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+const from = mek.key.remoteJid
+const type = Object.keys(mek.message)[0]        
+const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const typeQuoted = Object.keys(quoted)[0]
+const content = JSON.stringify(mek.message)
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+const body = mek.message.conversation || mek.message[type].caption || mek.message[type].text || ""
+chats = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
+budy = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
+
+if (prefix != "") {
+if (!body.startsWith(prefix)) {
+cmd = false
+comm = ""
+} else {
+cmd = true
+comm = body.slice(1).trim().split(" ").shift().toLowerCase()
+}
+} else {
+cmd = false
+comm = body.trim().split(" ").shift().toLowerCase()
+}
+        
+const command = comm
+
+const arg = chats.slice(command.length + 2, chats.length)
+const args = budy.trim().split(/ +/).slice(1)
+const isCmd = budy.startsWith(prefix)
+const q = args.join(' ')
+const soyYo = client.user.jid
+const botNumber = client.user.jid.split("@")[0]
+const ownerNumber = ['@s.whatsapp.net']
+const isGroup = from.endsWith('@g.us')
+const sender = mek.key.fromMe ? client.user.jid : isGroup ? mek.participant : mek.key.remoteJid
+const senderNumber = sender.split("@")[0]
+const isMe = senderNumber == botNumber
+const conts = mek.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = mek.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
+
+//AUTO RESPUESTA VERIFICADA
+if(body.includes('bot')) {
+client.sendMessage(from, 'Hola!', MessageType.text, {quoted: { key: {
+fromMe: false,
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+"documentMessage": { "title": "𝕿𝖍ٌ𝖊𝕮𝖍𝖔𝖚𝖙𝖊", 'jpegThumbnail': fs.readFileSync('./media/logo.jpg')}}
+}})
+}
+
+//AUTO RESPUESTA SIN VERIFICACION
+if(body == ('hola')) {
+client.sendMessage(from, 'como estas!', MessageType.text, {quoted: choute})
+}
+  
+if(body == ('Buenos dias')) {
+client.sendMessage(from, 'Solo Dias Porque Bueno Estoy Yo', MessageType.text, {quoted: choute})
+}
+
+//ZONA DE COMANDOS	
+switch (command) {
+case 'bot':
+client.sendMessage(from, 'Hola, felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted: { key: {
+fromMe: false,
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+"documentMessage": { "title": "𝕿𝖍ٌ𝖊𝕮𝖍𝖔𝖚𝖙𝖊", 'jpegThumbnail': fs.readFileSync('./media/logo.jpg')}}
+}})
+break		
+case 'foto':
+const imagen = fs.readFileSync('./media/foto.jpg')                
+client.sendMessage(from, imagen, MessageType.image, {quoted: choute, caption: `*Aqui tienes la foto del trio fundado*`})
+break
+                
+case 'video':
+const video = fs.readFileSync('./media/video.mp4')
+client.sendMessage(from, video, MessageType.video, {quoted: choute, mimetype: 'video/mp4', caption: 'JAJAJA', duration: 999999999})
+break
+                
+case 'audio':
+const audio = fs.readFileSync('./media/audio.mp3')
+client.sendMessage(from, audio, MessageType.audio, {quoted: choute, mimetype: 'audio/mp3', duration: -9999999, ptt: true})
+client.sendMessage(from, audio, MessageType.audio, {quoted: choute, mimetype: 'audio/mp3', duration: -9999999})                
+break
+                
+}
+
+} catch (e) {
+        
+console.log(e)}
+        
+})      
